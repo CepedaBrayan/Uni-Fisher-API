cd8e40699e446eb1189bfe40f287f4a2968bda48
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2023-11-30 19:01:23 -0500

    test

--- test/test_sample.py
+++ test/test_sample.py
@@ -4,4 +4,4 @@ from .setup import client
 def test_sample():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World :)"}
+    assert response.json() == {"Hello": "World"}
